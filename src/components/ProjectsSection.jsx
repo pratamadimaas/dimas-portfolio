999594f612e83d584c56fb11b5986fa9ff53de72
pratamadimaas <dimasmg2017@gmail.com>
2025-08-23 19:33:21 +0800

    ProjectsSection.jsx

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -241,7 +241,7 @@ const projects = [
         description: "A comprehensive accounting application built with the MERN stack for streamlined financial record-keeping, from general journals to financial reports.",
         images:["/images/22.png", "/images/23.png", "/images/24.png"],
         tags: ["React.js", "Express.js", "Node.js", "MongoDB"],
-        image: "/images/23.png",
+        image: "/images/22.png",
         publishedAt: "2025-08-23",
         summary: "A full-stack accounting application that automates financial workflows, including general journal entries, ledger management, and real-time financial reporting.",
         team: [{
